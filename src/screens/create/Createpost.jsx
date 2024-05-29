fcb6bd4f3cb459b5e2fc20fbf8d4ddc46ce4ad27
fmfahath <fmfahath.dev@gmail.com>
2024-05-29 12:28:47 +0530

    createpost page created+

--- src/screens/create/Createpost.jsx
+++ src/screens/create/Createpost.jsx
@@ -1,9 +1,50 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './Createpost.css'
+import useFetch from '../../hooks/useFetch'
 
 const Createpost = () => {
+
+    const [title, setTitle] = useState("")
+    const [content, setContent] = useState("")
+    const [validationError, setValidationError] = useState("")
+
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+
+        if (!title) {
+            setValidationError("Title is Must")
+            return
+        }
+        if (!content) {
+            setValidationError("Content is Must")
+            return
+        }
+
+        setValidationError('');
+        // console.log({ title, body: content, userId: 1 })
+
+    }
     return (
-        <div>Createpost</div>
+        <div className='container-createpost'>
+            {
+                validationError &&
+                <div className="alert alert-danger" role="alert">
+                    {validationError}
+                </div>
+            }
+            <form onSubmit={handleSubmit}>
+                <div>
+                    <label htmlFor="title">Title</label>
+                    <input type="text" id='title' name='title' value={title} onChange={(e) => setTitle(e.target.value)} />
+                </div>
+                <div>
+                    <label htmlFor="content">Content</label>
+                    <textarea name="content" id="content" rows='5' value={content} onChange={(e) => setContent(e.target.value)}></textarea>
+                </div>
+                <input type="submit" value='Create' />
+            </form>
+        </div>
     )
 }
 
