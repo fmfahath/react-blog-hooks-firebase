9375ee4e0ea63c3adf72a7660ec0c7f2c63bc5e1
fmfahath <fmfahath.dev@gmail.com>
2024-05-30 16:46:08 +0530

    read & display data from firestore

--- src/screens/home/Home.jsx
+++ src/screens/home/Home.jsx
@@ -1,47 +1,32 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import './Home.css'
 import PostCard from '../../components/postcard/PostCard';
+import { collection, getDocs } from 'firebase/firestore';
+import { db } from '../../firebase/config';
 
 
 
 const Home = () => {
-    const [data, setData] = useState([
-        {
-            "userId": 1,
-            "id": 1,
-            "title": "sunt aut facere repellat provident occaecati excepturi optio reprehenderit",
-            "body": "quia et suscipit\nsuscipit recusandae consequuntur expedita et cum\nreprehenderit molestiae ut ut quas totam\nnostrum rerum est autem sunt rem eveniet architecto"
-        },
-        {
-            "userId": 1,
-            "id": 2,
-            "title": "qui est esse",
-            "body": "est rerum tempore vitae\nsequi sint nihil reprehenderit dolor beatae ea dolores neque\nfugiat blanditiis voluptate porro vel nihil molestiae ut reiciendis\nqui aperiam non debitis possimus qui neque nisi nulla"
-        },
-        {
-            "userId": 1,
-            "id": 3,
-            "title": "ea molestias quasi exercitationem repellat qui ipsa sit aut",
-            "body": "et iusto sed quo iure\nvoluptatem occaecati omnis eligendi aut ad\nvoluptatem doloribus vel accusantium quis pariatur\nmolestiae porro eius odio et labore et velit aut"
-        },
-        {
-            "userId": 1,
-            "id": 4,
-            "title": "eum et est occaecati",
-            "body": "ullam et saepe reiciendis voluptatem adipisci\nsit amet autem assumenda provident rerum culpa\nquis hic commodi nesciunt rem tenetur doloremque ipsam iure\nquis sunt voluptatem rerum illo velit"
-        },
-        {
-            "userId": 1,
-            "id": 5,
-            "title": "nesciunt quas odio",
-            "body": "repudiandae veniam quaerat sunt sed\nalias aut fugiat sit autem sed est\nvoluptatem omnis possimus esse voluptatibus quis\nest aut tenetur dolor neque"
-        },
-    ])
+    const [data, setData] = useState([])
     const [error, setError] = useState("");
     const [isLoading, setIsLoading] = useState(false);
 
-    console.log("home..")
 
+    useEffect(() => {
+        const collectionRef = collection(db, "posts");
+
+        getDocs(collectionRef)
+            .then((snapshot) => {
+                let result = []
+
+                snapshot.docs.forEach((doc) => {
+                    result.push({ ...doc.data(), id: doc.id })
+                    setData(result)
+                })
+            })
+            .catch((err) => console.log(err))
+
+    }, [])
 
 
     return (
