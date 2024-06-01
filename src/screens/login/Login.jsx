5a3db9aa1839890393d72745555e2ab754b89d0f
fmfahath <fmfahath.dev@gmail.com>
2024-06-01 20:13:27 +0530

    login form created

--- src/screens/login/Login.jsx
+++ src/screens/login/Login.jsx
@@ -1,9 +1,63 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import './Login.css'
+import { useNavigate } from 'react-router-dom'
+import { useAuthentication } from '../../hooks/useAuthentication'
 
 const Login = () => {
+
+    const [email, setEmail] = useState("")
+    const [password, setPassword] = useState("")
+    const [validationError, setValidationError] = useState(null)
+    const navigate = useNavigate();
+
+    // const { signup, error, status } = useAuthentication();
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        // console.log("sumbit", { fname, lname, email, password })
+
+        // signup({ fname, lname, email, password });
+    }
+
+    // useEffect(() => {
+    //     if (status) {
+    //         const timer = setTimeout(() => {
+    //             navigate('/');
+    //         }, 3000);
+
+    //         return () => {
+    //             clearTimeout(timer)
+    //         }
+    //     }
+    // }, [status, error]);
+
     return (
-        <div>Login</div>
+        <div className='container-signup'>
+            <h2>Signin</h2>
+            {
+                validationError &&
+                <div className="alert alert-danger" role="alert">
+                    {validationError}
+                </div>
+            }
+            {/* {
+                error &&
+                <div className="alert alert-danger" role="alert">
+                    {error}
+                </div>
+            } */}
+            <form onSubmit={handleSubmit}>
+                <div>
+                    <label htmlFor='email'>Email</label>
+                    <input type='email' name='email' value={email} onChange={(e) => setEmail(e.target.value)} required />
+                </div>
+                <div>
+                    <label htmlFor='pwd'>Password</label>
+                    <input type='password' name='pwd' value={password} onChange={(e) => setPassword(e.target.value)} required />
+                </div>
+                <button type='submit'>Signin</button>
+            </form>
+        </div>
     )
 }
 
